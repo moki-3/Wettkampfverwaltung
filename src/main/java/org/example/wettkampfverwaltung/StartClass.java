5ce03f664c03a55cceac8faaf38735b3f3b68500
Moritz Zvonek <mokitemp92@gmail.com>
2025-12-10 13:59:21 +0100

    aus der CSV File kann jetzt ausgelesen werden, und aus dem inhalt wird eine scrollbare liste erstellt

--- src/main/java/org/example/wettkampfverwaltung/StartClass.java
+++ src/main/java/org/example/wettkampfverwaltung/StartClass.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.ScrollPane;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.VBox;
 import javafx.stage.FileChooser;
@@ -14,13 +15,19 @@ import java.util.ArrayList;
 import java.util.concurrent.atomic.AtomicReference;
 
 public class StartClass extends Application {
+    Wettkampf wf;
+    Stage controlStage;
+    Stage viewStage;
+
     @Override
     public void start(Stage stage) throws Exception {
         Label greeting = new Label("Hallo! Bitte wählen sie eine Datei aus");
         Button select = new Button("Datei auswählen");
         Button continueButton = new Button("Select a file to continue");
         continueButton.setDisable(true);
-        AtomicReference<ArrayList<FighterPair>> fighterPairs = new AtomicReference<>(new ArrayList<>());
+
+            AtomicReference<ArrayList<FighterPair>> fighterPairs = new AtomicReference<>(new ArrayList<>());
+
         select.setOnAction(actionEvent -> {
             continueButton.setText("Überprüfen...");
             FileChooser fc = new FileChooser();
@@ -34,8 +41,9 @@ public class StartClass extends Application {
             if (csv != null) {
                 System.out.println("File selected: " + csv.getAbsolutePath());
                 ReadFromCSV rfc = new ReadFromCSV();
-                fighterPairs.set(rfc.read(csv));
+                fighterPairs.set(rfc.read(csv)); // Hier wird die ArrayList gestzt
                 for (FighterPair pair : fighterPairs.get()) {
+                    //hier gebe ich die inhalte der fighterpairs arraylist aus
                     System.out.println("\n-------------------------------\n");
                     System.out.println(pair);
                 }
@@ -49,8 +57,8 @@ public class StartClass extends Application {
 
 
         continueButton.setOnAction(actionEvent -> {
-
-            //weiterImCode
+            wf = new Wettkampf(fighterPairs.get());
+            play();
         });
 
         VBox vbox = new VBox(10, greeting, select, continueButton);
@@ -62,9 +70,24 @@ public class StartClass extends Application {
         stage.setScene(scne);
 
         stage.setTitle("Neuer Wettkampf");
-        stage.show();
+        controlStage = stage;
+        controlStage.show();
+    }
 
+
+
+
+
+    private void listFights(){
+        Scene scn = new Scene(wf.createList());
+        controlStage.setTitle("Liste aller Kämpfe");
+        controlStage.setScene(scn);
+    }
+
+    public void play(){
+        listFights();
 
 
     }
+
 }
---
+++ src/main/java/org/example/wettkampfverwaltung/Wettkampf.java
@@ -0,0 +1,83 @@
+package org.example.wettkampfverwaltung;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+import javafx.scene.control.Label;
+import java.util.ArrayList;
+
+public class Wettkampf {
+
+    private ArrayList<FighterPair> fighterPairs = new ArrayList<>();
+
+    public ArrayList<FighterPair> getFighterPairs() {
+        return fighterPairs;
+    }
+
+    public Wettkampf(ArrayList<FighterPair> list){
+        this.fighterPairs = list;
+    }
+
+    public ScrollPane createList() {
+        VBox vbox = new VBox();
+        for (FighterPair fp : fighterPairs) {
+//            Button button = new Button(fp.getName01() + " (" + fp.getVerein01() + ") " + " | "
+//                    + fp.getName02() + " (" + fp.getVerein02() + ") " + " | " + fp.getAltersKlasse() + fp.getGewichtsKlasse());
+            Label name01 = new Label(fp.getName01());
+            Label name02 = new Label(fp.getName02());
+            Label verein01 = new Label("(" + fp.getVerein01() + ")");
+            Label verein02 = new Label("(" + fp.getVerein02() + ")");
+            Label altersklasse = new Label(fp.getAltersKlasse());
+            Label gewichtsklasse = new Label(fp.getGewichtsKlasse());
+            if (!fp.getWinner().equals("nicht gesetzt")) {
+                //wenn es einen gewinner gibt, css klassen setzten
+                if (fp.getWinner().equals(name01.getText())) {
+                    /*
+                        CSS klassen: schrift bei name01 und verein01 grün, und bei name02 und verein02 rot machen
+                     */
+                } else {
+                    /*
+                        CSS klassen: schrift bei name02 und verein02 grün, und bei name01 und verein01 rot machen
+                     */
+                }
+
+            }
+
+            HBox row01 = new HBox();
+            HBox row02 = new HBox();
+            HBox row03 = new HBox();
+            row01.getChildren().addAll(name01, verein01);
+            row01.setSpacing(30);
+            row02.getChildren().addAll(name02, verein02);
+            row02.setSpacing(30);
+            row03.getChildren().addAll(altersklasse, gewichtsklasse);
+            row03.setSpacing(50);
+
+
+            //css klassen für die hboxes
+
+
+            VBox tmp = new VBox(10, row01, row02, row03);
+
+            //css klassen
+
+            tmp.setOnMouseClicked(mouseEvent -> {
+                System.out.println("vbox geklickt");
+            });
+
+            vbox.getChildren().add(tmp);
+
+
+        }
+
+        ScrollPane sp = new ScrollPane(vbox);
+        sp.setVbarPolicy(ScrollPane.ScrollBarPolicy.ALWAYS); // immer vertikal scrollen
+        sp.setHbarPolicy(ScrollPane.ScrollBarPolicy.AS_NEEDED); //bei bedarf horizonal scrollen
+        return sp;
+    }
+}
